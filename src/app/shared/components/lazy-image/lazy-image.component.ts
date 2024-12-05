fabc5ec74a451ac22a0afe0d92cad321771c7813
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-05 19:17:32 -0300

    3. Video 108 Finalizado

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  standalone: false,
+
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent {
+
+}
