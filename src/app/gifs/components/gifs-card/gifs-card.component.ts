f46c125567131095cc538dacf3491c61eb8b695b
Alexis Andrés Barría Cabello <alexis.barria.c@outlook.cl>
2024-12-05 17:36:14 -0300

    1. Video 106 Finalizado

---
+++ src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-card',
+  standalone: false,
+  
+  templateUrl: './gifs-card.component.html',
+  styleUrl: './gifs-card.component.css'
+})
+export class GifsCardComponent {
+
+}
